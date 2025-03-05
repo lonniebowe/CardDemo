c4e04aa2131caa2bd71ced3c19b1f5529291c796
ethanboard <136537807+ethanboard@users.noreply.github.com>
2025-03-05 13:41:23 -0500

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,1 +1,23 @@
+from playing_card import PlayingCard
+import random
+
+class Deck:
+    """A deck of playing cards"""
+
+    def __init__(self):
+        self.cards = [PlayingCard(rank, suit) for suit in PlayingCard.SUITS for rank in PlayingCard.RANKS]
+    
+    def shuffle(self):
+        """Shuffles the deck"""
+        random.shuffle(self.cards)
+    
+    def draw(self):
+        """Draws a card from the deck"""
+        if not self.cards:
+            raise ValueError("No cards left in the deck")
+        return self.cards.pop()
+    
+    def __len__(self):
+        """Returns the number of cards left in the deck"""
+        return len(self.cards)
 
