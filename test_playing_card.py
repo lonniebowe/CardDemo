6b74a51058ed9dd3e34cea867ce13ccb15504428
Bryson <148238289+brysongroves@users.noreply.github.com>
2025-03-05 14:08:02 -0500

    Update test_playing_card.py

--- test_playing_card.py
+++ test_playing_card.py
@@ -1,6 +1,5 @@
 import unittest
-
-from playing_card import PlayingCard
+from playing_card import PlayingCard, Deck, Hand
 
 class TestPlayingCard(unittest.TestCase):
 
@@ -25,6 +24,54 @@ class TestPlayingCard(unittest.TestCase):
         card1 = PlayingCard("10", "Diamonds")
         card2 = PlayingCard("J", "Diamonds")
         self.assertNotEqual(card1, card2)
+
+class TestDeck(unittest.TestCase):
+
+    def test_deck_size(self):
+        deck = Deck()
+        self.assertEqual(len(deck.cards), 52)
+
+    def test_deck_shuffle(self):
+        deck1 = Deck()
+        deck2 = Deck()
+        deck1.shuffle()
+        self.assertNotEqual(deck1.cards, deck2.cards)  # Shuffled deck should be different
+
+    def test_draw_card(self):
+        deck = Deck()
+        initial_size = len(deck.cards)
+        card = deck.draw_card()
+        self.assertIsInstance(card, PlayingCard)
+        self.assertEqual(len(deck.cards), initial_size - 1)
+
+    def test_draw_from_empty_deck(self):
+        deck = Deck()
+        for _ in range(52):
+            deck.draw_card()
+        with self.assertRaises(ValueError):
+            deck.draw_card()
+
+class TestHand(unittest.TestCase):
+
+    def test_add_card(self):
+        hand = Hand()
+        card = PlayingCard("Q", "Spades")
+        hand.add_card(card)
+        self.assertIn(card, hand.cards)
+
+    def test_display_hand(self):
+        hand = Hand()
+        card1 = PlayingCard("J", "Clubs")
+        card2 = PlayingCard("7", "Hearts")
+        hand.add_card(card1)
+        hand.add_card(card2)
+        self.assertEqual(hand.display_hand(), ["J of Clubs", "7 of Hearts"])
+
+    def test_number_of_cards(self):
+        hand = Hand()
+        self.assertEqual(hand.number_of_cards(), 0)
+        hand.add_card(PlayingCard("5", "Diamonds"))
+        self.assertEqual(hand.number_of_cards(), 1)
 
 if __name__ == "__main__":
     unittest.main()
