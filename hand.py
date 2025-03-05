fd4ca0ed62f3aed8d5355fcb5cf30fee6d1c3a4f
ethanboard <136537807+ethanboard@users.noreply.github.com>
2025-03-05 13:42:11 -0500

    Update hand.py

--- hand.py
+++ hand.py
@@ -1,1 +1,28 @@
+from playing_card import PlayingCard
+
+class Hand:
+    """A hand of playing cards"""
+
+    def __init__(self):
+        self.cards = []
+    
+    def add_card(self, card):
+        """Adds a card to the hand"""
+        if not isinstance(card, PlayingCard):
+            raise ValueError("Can only add PlayingCard instances")
+        self.cards.append(card)
+    
+    def remove_card(self, card):
+        """Removes a card from the hand"""
+        if card in self.cards:
+            self.cards.remove(card)
+        else:
+            raise ValueError("Card not in hand")
+
+    def __len__(self):
+        """Returns the number of cards in hand"""
+        return len(self.cards)
+
+    def __str__(self):
+        return ", ".join(str(card) for card in self.cards)
 
